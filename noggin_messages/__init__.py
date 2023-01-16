a0f54d8696b8159a871dbd8b771f318457537639
Ryan Lerch <rlerch@redhat.com>
2023-01-16 21:11:59 +1000

    Update noggin schemas
    Adds a bunch of missing properties like summary, agent_name, __str__,
    and app_name that are needed for upcoming FMN work
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- noggin_messages/__init__.py
+++ noggin_messages/__init__.py
@@ -1,8 +1,99 @@
 from fedora_messaging import message
+from fedora_messaging.schema_utils import user_avatar_url
 
 
-class MemberSponsorV1(message.Message):
+class NogginMessage(message.Message):
+    @property
+    def app_name(self):
+        """
+        Return the name of the application that generated the message.
+
+        Returns:
+            the name of the application (fas)
+        """
+        return "fas"
+
+    @property
+    def agent_name(self):
+        """Return the agent's username for this message.
+
+        Returns:
+            The agent's username, or None if the msg has no agent key.
+        """
+        return self.body["msg"]["agent"]
+
+    @property
+    def user_name(self):
+        """Return the username for this message.
+
+        Returns:
+            The username this message is about
+        """
+        return self.body["msg"]["user"]
+
+    @property
+    def agent_avatar(self):
+        """
+        Return a URL to the avatar of the user who caused the action.
+
+        Returns:
+            The URL to the user's avatar, or None if username is None.
+        """
+        return user_avatar_url(self.agent_name)
+
+    @property
+    def usernames(self):
+        """
+        List of users affected by the action that generated this message.
+
+        Returns:
+            A list of affected usernames.
+        """
+        users = [self.agent_name, self.user_name]
+        users.sort()
+        return users
+
+    def __str__(self):
+        """
+        Return a human-readable representation of this message.
+
+        This should provide a detailed representation of the message, much like the body
+        of an email.
+
+        Returns:
+            A human readable representation of this message.
+        """
+        return self.summary
+
+
+class MemberSponsorV1(NogginMessage):
     """The message sent when a user is added to a group by a sponsor"""
+
+    @property
+    def groups(self):
+        """
+        List of groups affected by the action that generated this message.
+
+        Returns:
+            A list of affected groups.
+        """
+        return [self.body["msg"]["group"]]
+
+    @property
+    def summary(self):
+        """
+        Return a short, human-readable representation of this message.
+
+        This should provide a short summary of the message, much like the subject line
+        of an email.
+
+        Returns:
+            A summary for this message.
+        """
+        return (
+            f"Sponsor {self.agent_name} added {self.user_name} to the group"
+            f"{self.groups[0]}"
+        )
 
     topic = "fas.group.member.sponsor"
     body_schema = {
@@ -26,8 +117,27 @@ class MemberSponsorV1(message.Message):
     }
 
 
-class UserCreateV1(message.Message):
+class UserCreateV1(NogginMessage):
     """The message sent when a user is created"""
+
+    @property
+    def summary(self):
+        """
+        Return a short, human-readable representation of this message.
+
+        This should provide a short summary of the message, much like the subject line
+        of an email.
+
+        Returns:
+            A summary for this message.
+        """
+
+        if self.user_name == self.agent_name:
+            return f"{self.agent_name} created a new Fedora Account"
+        else:
+            return (
+                f"{self.agent_name} created a new Fedora Account for {self.user_name}"
+            )
 
     topic = "fas.user.create"
     body_schema = {
@@ -50,8 +160,48 @@ class UserCreateV1(message.Message):
     }
 
 
-class UserUpdateV1(message.Message):
+class UserUpdateV1(NogginMessage):
     """The message sent when a user is updated"""
+
+    @property
+    def summary(self):
+        """
+        Return a short, human-readable representation of this message.
+
+        This should provide a short summary of the message, much like the subject line
+        of an email.
+
+        Returns:
+            A summary for this message.
+        """
+
+        if self.user_name == self.agent_name:
+            return (
+                f"{self.agent_name} edited {len(self.body['msg']['fields'])} details of "
+                "their Fedora Account"
+            )
+        else:
+            return (
+                f"{self.agent_name} edited {len(self.body['msg']['fields'])} details of "
+                f"{self.user_name}'s Fedora Account"
+            )
+
+    def __str__(self):
+        """
+        Return a human-readable representation of this message.
+
+        This should provide a detailed representation of the message, much like the body
+        of an email.
+
+        Returns:
+            A human readable representation of this message.
+        """
+        new_line = "\n"
+        return (
+            f"{self.summary}\n\n"
+            f"Details changed:\n\n"
+            f"{new_line.join([item for item in self.body['msg']['fields']])} "
+        )
 
     topic = "fas.user.update"
     body_schema = {
--- noggin_messages/tests/test_schemas.py
+++ noggin_messages/tests/test_schemas.py
@@ -37,6 +37,20 @@ class TestSchema:
         )
         msg.validate()
 
+        assert msg.app_name == "fas"
+        assert msg.agent_name == "dudemcpants"
+        assert msg.usernames == ["dudemcpants", "testuser"]
+        assert msg.agent_avatar == (
+            "https://seccdn.libravatar.org/avatar/caa750e"
+            "df4a11206831a58713cf9231b5b3227765887cbc765d4"
+            "f8c5c55576a5?s=64&d=retro"
+        )
+        assert msg.groups == ["developers"]
+        assert (
+            msg.summary == "Sponsor dudemcpants added testuser to the groupdevelopers"
+        )
+        assert str(msg) == msg.summary
+
         msg_dump = json.loads(message.dumps(msg))
         assert msg_dump["body"]["msg"]["agent"] == "dudemcpants"
         assert msg_dump["body"]["msg"]["user"] == "testuser"
@@ -75,11 +89,28 @@ class TestSchema:
         msg = UserCreateV1({"msg": {"agent": "dudemcpants", "user": "testuser"}})
         msg.validate()
 
+        assert msg.app_name == "fas"
+        assert msg.agent_name == "dudemcpants"
+        assert msg.usernames == ["dudemcpants", "testuser"]
+        assert msg.agent_avatar == (
+            "https://seccdn.libravatar.org/avatar/caa750e"
+            "df4a11206831a58713cf9231b5b3227765887cbc765d4"
+            "f8c5c55576a5?s=64&d=retro"
+        )
+        assert msg.groups == []
+        assert msg.summary == "dudemcpants created a new Fedora Account for testuser"
+        assert str(msg) == msg.summary
+
         msg_dump = json.loads(message.dumps(msg))
         assert msg_dump["body"]["msg"]["agent"] == "dudemcpants"
         assert msg_dump["body"]["msg"]["user"] == "testuser"
         assert msg_dump["headers"]["fedora_messaging_schema"] == "noggin.user.create.v1"
         assert msg_dump["topic"] == "fas.user.create"
+
+        # the summary is different if agent == user, so lets test that too
+        msg2 = UserCreateV1({"msg": {"agent": "dudemcpants", "user": "dudemcpants"}})
+        msg2.validate()
+        assert msg2.summary == "dudemcpants created a new Fedora Account"
 
     def test_user_update_v1(self):
         """
@@ -118,6 +149,28 @@ class TestSchema:
         )
         msg.validate()
 
+        assert msg.app_name == "fas"
+        assert msg.agent_name == "dudemcpants"
+        assert msg.usernames == ["dudemcpants", "testuser"]
+        assert msg.agent_avatar == (
+            "https://seccdn.libravatar.org/avatar/caa750e"
+            "df4a11206831a58713cf9231b5b3227765887cbc765d4"
+            "f8c5c55576a5?s=64&d=retro"
+        )
+        assert msg.groups == []
+        assert (
+            msg.summary == "dudemcpants edited 3 details of testuser's Fedora Account"
+        )
+        assert str(msg) == (
+            "dudemcpants edited 3 details of testuser's Fedora Account\n"
+            "\n"
+            "Details changed:\n"
+            "\n"
+            "firstname\n"
+            "lastname\n"
+            "gpgkeyid "
+        )
+
         msg_dump = json.loads(message.dumps(msg))
         assert msg_dump["body"]["msg"]["agent"] == "dudemcpants"
         assert msg_dump["body"]["msg"]["user"] == "testuser"
@@ -128,3 +181,24 @@ class TestSchema:
         ]
         assert msg_dump["headers"]["fedora_messaging_schema"] == "noggin.user.update.v1"
         assert msg_dump["topic"] == "fas.user.update"
+
+        # the summary and __str__  is different if agent == user, so lets test that too
+        msg2 = UserUpdateV1(
+            {
+                "msg": {
+                    "agent": "dudemcpants",
+                    "user": "dudemcpants",
+                    "fields": ["firstname", "lastname"],
+                }
+            }
+        )
+        msg2.validate()
+        assert msg2.summary == "dudemcpants edited 2 details of their Fedora Account"
+        assert str(msg2) == (
+            "dudemcpants edited 2 details of their Fedora Account\n"
+            "\n"
+            "Details changed:\n"
+            "\n"
+            "firstname\n"
+            "lastname "
+        )
